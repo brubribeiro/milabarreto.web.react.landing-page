9078e901496309dbf9fc4b3e255c2be3e797d207
Bruna Ribeiro <bruna.ribeiro@facens.br>
2024-11-23 18:42:55 -0300

    feature(footer): added footer

---
+++ src/app/sections/footer/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="bg-primary text-white py-8 px-4 text-center animate-fadeIn_1_5"> 
+      <p className="text-sm">© 2024 Mila Barreto Estética Avançada. Todos os direitos reservados.</p>
+    </footer>
+  );
+}

4f15b0f0b8be31b9829c5d0d966cc1f479321a0b
Bruna Ribeiro <bruna.ribeiro@facens.br>
2024-11-23 18:44:26 -0300

    feature(page): added new structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 import { AboutUs } from '@/app/sections/about-us/about-us';
 import { Welcome } from '@/app/sections/welcome/welcome';
+import { Location } from '@/app/sections/location/location';
 import { Navbar } from './components/navbar/navbar';
+import { WhatsAppFloat } from './components/whatsapp-float/whatsapp-float';
+import { Footer } from './sections/footer/footer';
+import { Ratings } from './sections/ratings/ratings';
+import { Procedures } from './sections/procedures/procedures';
+import { Contact } from './sections/contact/contact';
 
 export default function Home() {
   return (
     <>
+      <WhatsAppFloat />
       <Navbar />
       <Welcome/>
       <AboutUs/>
+      <Procedures />
+      <Ratings/>
+      <Location />
+      <Contact />
+      <Footer />
     </>
   );
 }

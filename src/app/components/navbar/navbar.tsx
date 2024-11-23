d4909855ecf202e0ab75bc66a8675adb9cafb956
Bruna Ribeiro <bruna.ribeiro@facens.br>
2024-11-23 18:41:01 -0300

    refactor(navbar): added logo

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,19 +1,39 @@
+import Image from "next/image";
 export function Navbar() {
+
+  const links = [
+    { href: "#inicio", text: "Início" },
+    { href: "#sobre", text: "Sobre" },
+    { href: "#procedimentos", text: "Procedimentos" },
+    { href: "#profissionais", text: "Profissionais" },
+    { href: "#avaliacoes", text: "Avaliações" },
+    { href: "#localizacao", text: "Localização" },
+    { href: "#contato", text: "Contato" },
+  ];
+
+  const NavLink = ({ href, text }: { href: string; text: string }) => (
+    <a
+      href={href}
+      className="text-white font-semibold relative transition-all duration-300 ease-in-out pb-[5px] hover:text-white 
+        after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:bottom-0 
+        after:left-1/2 after:transform after:-translate-x-1/2 after:transition-all after:duration-300 
+        after:ease-in-out hover:after:w-full">
+      {text}
+    </a>
+  );
+
   return (
-    <nav className="fixed w-full shadow-md px-12 py-8 bg-primary flex justify-between items-center z-50">
+    <nav className="fixed w-full shadow-md px-12 h-[96px] bg-primary flex justify-between items-center z-50">
     <div className="text-white text-2xl font-bold">
-        <h2>Estética Avançada</h2>
+        <Image src={"/milabarreto_logo.png"} alt="Logo da clínica" width={200} height={50} />
     </div>
-    <div className="flex gap-8">
-        <a className="text-white font-semibold relative transition-all duration-300 ease-in-out pb-[5px] hover:text-white after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full" href="#inicio">Início</a>
-        <a className="text-white font-semibold relative transition-all duration-300 ease-in-out pb-[5px] hover:text-white after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full" href="#sobre">Sobre</a>
-        <a className="text-white font-semibold relative transition-all duration-300 ease-in-out pb-[5px] hover:text-white after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full" href="#fotos">Fotos</a>
-        <a className="text-white font-semibold relative transition-all duration-300 ease-in-out pb-[5px] hover:text-white after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full" href="#procedimentos">Procedimentos</a>
-        <a className="text-white font-semibold relative transition-all duration-300 ease-in-out pb-[5px] hover:text-white after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full" href="#profissionais">Profissionais</a>
-        <a className="text-white font-semibold relative transition-all duration-300 ease-in-out pb-[5px] hover:text-white after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full" href="#avaliacoes">Avaliações</a>
-        <a className="text-white font-semibold relative transition-all duration-300 ease-in-out pb-[5px] hover:text-white after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full" href="#localizacao">Localização</a>
-        <a className="text-white font-semibold relative transition-all duration-300 ease-in-out pb-[5px] hover:text-white after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:bottom-0 after:left-1/2 after:transform after:-translate-x-1/2 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full" href="#contato">Contato</a>
-    </div>
+    <ul className="flex gap-8">
+        {links.map((link) => (
+          <li key={link.href}>
+            <NavLink href={link.href} text={link.text} />
+          </li>
+        ))}
+      </ul>
 </nav>
   )
 }
